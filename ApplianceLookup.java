36e3c5629c32cb9012cf3aa04ecff9b29846f0c9
Alexis Manosca <alexismanosca@gmail.com>
2025-04-10 16:15:10 +1200

    Set up main menu which reads user input

--- ApplianceLookup.java
+++ ApplianceLookup.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class ApplianceLookup{
     public static void main(String[] args){
@@ -20,8 +21,8 @@ public class ApplianceLookup{
             // While the application is running
             while(isRunning){
 
-                // Store the header then read the next line
-                String header = reader.readLine();
+                // Read the header then read the next line
+                reader.readLine();
                 line = reader.readLine();
                 
                 // Read the entire csv file
@@ -40,16 +41,67 @@ public class ApplianceLookup{
                 // List out the tree values
                 applianceBST.print();
 
-                // Stops the program
-                isRunning = false;
+                // Initalise user input
+                int userinputer = 0;
+                Scanner scanner = new Scanner(System.in);
+
+                // Print out the list of console functions that the user can run
+                System.out.println("--------------------");
+                System.out.println("Main menu: ");
+                System.out.println("1. Search for an appliance");
+                System.out.println("2. Add a new appliance");
+                System.out.println("3. Remove an existing appliance");
+                System.out.println("4. Display all appliances within a category");
+                System.out.println("5. Display all appliances within a category in the same price range");
+                System.out.println("6. Display all appliances within a category above a minimum price");
+                System.out.println("7. Display all appliances within a category below a minimum price");
+                System.out.println("8. Exit the program");
+                System.out.print("Enter a number that corresponds to your request: ");
+                int userInput = Integer.parseInt(scanner.nextLine());
+
+                // Searches for an appliance
+                if (userInput == 1){
+
+                    System.out.println("--------------------");
+                    System.out.println("Searching an appliance...");
+
+                    // Get data from user
+                    System.out.print("Enter category: ");
+                    String category = scanner.nextLine();
+                    System.out.print("Enter name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter price: ");
+                    float price = Float.parseFloat(scanner.nextLine());
+
+                    // Make a new appliance and try to find an appliance with the same specs in the bst
+                    Appliance newAppliance = new Appliance(category, price, name);
+                    boolean applianceFound = applianceBST.search(newAppliance);
+
+                    // Inform user if appliance was found
+                    if (applianceFound){
+                        System.out.println("Appliance was found in the system.");
+                        System.out.println(newAppliance.toString());
+                    }
+                    else{
+                        System.out.println("Appliance was not found in the system. Please make sure that spelling is correct. Input is case-sensitive.");
+                    }
+
+                    // Return or quit
+                    System.out.println("Returning to main menu...");
+                    Thread.sleep(300);
+                }
+
+                // Close the scanner
+                //scanner.close();
+
             }
 
-            // Close the reader
+            // Close the reader 
             reader.close();
             
         }
         catch (Exception e){
-            System.out.println("An error occured: " + e.getMessage());
+            System.out.println("An error occured. " + e.getMessage());
         }
 
     }
